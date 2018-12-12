acb81f66e569b96a34ca66b817c6e95670f008a3
Stepan <stepos36@gmail.com>
2018-12-11 22:19:26 -0500

    animate/pause on click functioning

--- assets/javascript/javascript.js
+++ assets/javascript/javascript.js
@@ -12,18 +12,18 @@ function renderButtons() {
 };
 function displayGifs(){
     var car = $(this).attr('data-name');
-    var queryURL = "https://api.giphy.com/v1/gifs/search?api_key=uVCLVNVcBBEYH6a1LY4mpc6ZOLUNMlzA&q=" + car + "&limit=10&offset=0&rating=G&lang=en"
+    var queryURL = "https://api.giphy.com/v1/gifs/search?api_key=uVCLVNVcBBEYH6a1LY4mpc6ZOLUNMlzA&q=" + car + "&limit=10&offset=0&rating=G&lang=en";
     $.ajax({
         url: queryURL,
         method: "GET"
     }).then(function(response) {
-        var result = response.data
+        var result = response.data;
         for (var i=0;i<result.length;i++) {
             var gif = $('<img>');
-            gif.attr('src', result[i].images.fixed_height.url).attr('class', car).attr('style', 'margin: 5px 5px').css('border', '2px groove #560000')
-            $('#gif-display').prepend(gif)
-            
-            console.log('gif')
+            var still = result[i].images.fixed_height_still.url
+            var animated = result[i].images.fixed_height.url
+            gif.attr('src', still).attr('data-animated', animated).attr('data-still', still).attr('state', 'still').attr('class', car).attr('class', 'gifPic').attr('style', 'margin: 5px 5px').css('border', '2px groove #560000');
+            $('#gif-display').prepend(gif);
         }
     });
 }
@@ -31,15 +31,20 @@ function displayGifs(){
 
 $(document).ready(function() {
     renderButtons()
-    $(document).on("click", ".car-btn", displayGifs)
+    $(document).on("click", ".car-btn", displayGifs);
     $(document).on('click', '#submitter', function(event) {
         event.preventDefault();
-        var newCar = $('#car-input').val().trim()
-        cars.push(newCar)
-        renderButtons()
-        
+        var newCar = $('#car-input').val().trim();
+        cars.push(newCar);
+        renderButtons();
     });
-
-
-    console.log('done')
+    $(document).on('click', '.gifPic', function() {
+        if ($(this).attr('state') === "still") {
+            $(this).attr("src", $(this).attr("data-animated"));
+            $(this).attr("state", "animate");
+          } else {
+            $(this).attr("src", $(this).attr("data-still"));
+            $(this).attr("state", "still");
+          }
+        });
 });
