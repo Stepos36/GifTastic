f3324a74a0e6a781ec258db79b34aa3337ecc09b
Stepan <stepos36@gmail.com>
2018-12-11 23:23:51 -0500

    styling

--- assets/javascript/javascript.js
+++ assets/javascript/javascript.js
@@ -6,13 +6,13 @@ function renderButtons() {
         button.addClass('car-btn');
         button.attr('data-name', cars[i]);
         button.text(cars[i]);
-        $('#buttons').append(button);
-        $('.car-btn').css('margin', '5px 5px')
+        $('#buttons').append(button).attr('style', 'text-align:center;display: flex; justify-content: center');
+        $('.car-btn').attr('style', 'display: inline-block; margin: 5px 5px; ');
     }
 };
 function displayGifs(){
     var car = $(this).attr('data-name');
-    var queryURL = "https://api.giphy.com/v1/gifs/search?api_key=uVCLVNVcBBEYH6a1LY4mpc6ZOLUNMlzA&q=" + car + "&limit=10&offset=0&rating=G&lang=en";
+    var queryURL = "https://api.giphy.com/v1/gifs/search?api_key=uVCLVNVcBBEYH6a1LY4mpc6ZOLUNMlzA&q=" + car + "&limit=15&offset=0&rating=G&lang=en";
     $.ajax({
         url: queryURL,
         method: "GET"
@@ -22,8 +22,8 @@ function displayGifs(){
             var gif = $('<img>');
             var still = result[i].images.fixed_height_still.url
             var animated = result[i].images.fixed_height.url
-            gif.attr('src', still).attr('data-animated', animated).attr('data-still', still).attr('state', 'still').attr('class', car).attr('class', 'gifPic').attr('style', 'margin: 5px 5px').css('border', '2px groove #560000');
-            $('#gif-display').prepend(gif);
+            gif.attr('src', still).attr('data-animated', animated).attr('data-still', still).attr('state', 'still').attr('class', car).attr('class', 'gifPic').attr('style', 'margin: 5px 3px').css('border', '3px groove #560000').css('border-radius', '10px').css('display', 'inline-block');
+            $('#gif-display').prepend(gif).css('text-align', 'center');
         }
     });
 }
@@ -37,6 +37,7 @@ $(document).ready(function() {
         var newCar = $('#car-input').val().trim();
         cars.push(newCar);
         renderButtons();
+        $('#car-input').val('')
     });
     $(document).on('click', '.gifPic', function() {
         if ($(this).attr('state') === "still") {
