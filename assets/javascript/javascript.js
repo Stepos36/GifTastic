9ad276ef1b01467ccb5b1d04a8a244fdd96ccc2d
Stepan <stepos36@gmail.com>
2018-12-11 21:20:59 -0500

    displayGifs function working with ajax

--- assets/javascript/javascript.js
+++ assets/javascript/javascript.js
@@ -9,9 +9,30 @@ function renderButtons() {
         $('#buttons').append(button);
     }
 };
+function displayGifs(){
+    var car = $(this).attr('data-name');
+    var queryURL = "https://api.giphy.com/v1/gifs/search?api_key=uVCLVNVcBBEYH6a1LY4mpc6ZOLUNMlzA&q=" + car + "&limit=10&offset=0&rating=G&lang=en"
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    }).then(function(response) {
+        var result = response.data
+        for (var i=0;i<result.length;i++) {
+            var gifDiv = $('<div>')
+            var gif = $('<img>');
+            gif.attr('src', result[i].images.fixed_width.url)
+            gifDiv.append(gif)
+            $('#gif-display').prepend(gifDiv)
+            console.log('gif')
+        }
+    });
+}
+
+
 $(document).ready(function() {
 renderButtons()
 $('.car-btn').css('margin', '5px 5px')
+$(document).on("click", ".car-btn", displayGifs)
 
 console.log('done')
-})
+});
